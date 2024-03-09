25ee3ae546bab083bcbd2d7aaea09695f051f331
Magnus Åberg <himself@magnusaberg.me>
2024-03-09 20:21:04 +0100

    Add retrospecitve log check

--- spider.sh
+++ spider.sh
@@ -5,68 +5,74 @@ URL=""
 RESPONSE_CODE="404"
 
 print_title() {
-  echo -e "\033[0;32m░▒▓█▓▒░░▒▓█▓▒░░▒▓████████▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓███████▓▒░░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓███████▓▒░ ░▒▓████████▓▒░░▒▓███████▓▒░  ";
-  echo -e "\033[0;32m░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░ ";
-  echo -e "\033[0;32m░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░ ";
-  echo -e "\033[0;32m░▒▓████████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓████████▓▒░       ░▒▓██████▓▒░ ░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░  ░▒▓███████▓▒░  ";
-  echo -e "\033[0;32m       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░ ";
-  echo -e "\033[0;32m       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░ ";
-  echo -e "\033[0;32m       ░▒▓█▓▒░░▒▓████████▓▒░       ░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓█▓▒░       ░▒▓█▓▒░░▒▓███████▓▒░ ░▒▓████████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ";
-  echo -e "\033[0;32m                                                                                                                              ";
-  echo -e "\033[0m                                                                                                                              ";
+	echo -e "\033[0;32m░▒▓█▓▒░░▒▓█▓▒░░▒▓████████▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓███████▓▒░░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓███████▓▒░ ░▒▓████████▓▒░░▒▓███████▓▒░  "
+	echo -e "\033[0;32m░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░ "
+	echo -e "\033[0;32m░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░ "
+	echo -e "\033[0;32m░▒▓████████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓████████▓▒░       ░▒▓██████▓▒░ ░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░  ░▒▓███████▓▒░  "
+	echo -e "\033[0;32m       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░ "
+	echo -e "\033[0;32m       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░ "
+	echo -e "\033[0;32m       ░▒▓█▓▒░░▒▓████████▓▒░       ░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓█▓▒░       ░▒▓█▓▒░░▒▓███████▓▒░ ░▒▓████████▓▒░░▒▓█▓▒░░▒▓█▓▒░ "
+	echo -e "\033[0;32m                                                                                                                              "
+	echo -e "\033[0m                                                                                                                              "
 }
 
 print_help() {
-  echo "Usage: $0 [OPTIONS]"
-  echo "This bash script is used to perform a wget operation on a specified URL, logging the output to a specified file. "
-  echo "Options: "
-  echo "  --output         Specify the output file to which the log will be written."
-  echo "  --url            Specify the URL to be scanned with wget."
-  echo "  --response-code  Specify the HTTP response code to be targeted in the log output."
-  echo "  --help           Print this help message and exit."
+	echo "Usage: $0 [OPTIONS]"
+	echo "This bash script is used to perform a wget operation on a specified URL, logging the output to a specified file. "
+	echo "Options: "
+	echo "  --output         Specify the output file to which the log will be written."
+	echo "  --url            Specify the URL to be scanned with wget."
+	echo "  --response-code  Specify the HTTP response code to be targeted in the log output."
+	echo "  --help           Print this help message and exit."
 }
 
-while (( "$#" )); do
-  case "$1" in
-    --output)
-      OUTPUT="$2"
-      shift 2
-      ;;
-    --url)
-      URL="$2"
-      shift 2
-      ;;
-    --response-code)
-      RESPONSE_CODE="$2"
-      shift 2
-      ;;
-    --help)
-      print_help
-      exit 0
-      ;;
-    *)
-      shift
-      ;;
-  esac
+while (("$#")); do
+	case "$1" in
+	--output)
+		OUTPUT="$2"
+		shift 2
+		;;
+	--url)
+		URL="$2"
+		shift 2
+		;;
+	--response-code)
+		RESPONSE_CODE="$2"
+		shift 2
+		;;
+	--help)
+		print_help
+		exit 0
+		;;
+	*)
+		shift
+		;;
+	esac
 done
 
 if hash !wget 2>/dev/null; then
-   echo "wget is not installed. Please install."
-   exit 1
+	echo "wget is not installed. Please install."
+	exit 1
 fi
 
-print_title;
+print_title
 
 if [ -n "$OUTPUT" ] && [ -n "$URL" ] && [ -n "$RESPONSE_CODE" ]; then
-  wget --spider \
-    --output-file "$OUTPUT" \
-    --execute robots=off \
-    --recursive \
-    --page-requisites \
-    --no-directories \
-    --no-check-certificate \
-    "$URL"
+	wget --spider \
+		--output-file "$OUTPUT" \
+		--execute robots=off \
+		--recursive \
+		--page-requisites \
+		--no-directories \
+		--no-check-certificate \
+		"$URL"
 
-  tput setab 1; tput setaf 2;
-  grep -B 2 " ${RESPONSE_CODE} .*" "$OUTPUT" | awk 'NR%2==1'; tput sgr0
 fi
+
+if [ -n "$RESPONSE_CODE" ] && [ -n "$OUTPUT" ]; then
+	tput setab 1
+	tput setaf 2
+	grep -B 2 " ${RESPONSE_CODE} .*" "$OUTPUT" | awk 'NR%2==1'
+	tput sgr0
+fi
+
